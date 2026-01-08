8a4a3a547a5f590483528331da504f4270693f0c
lijy78 <lijy78@asiainfo.com>
2026-01-08 11:31:52 +0800

    更改注释

--- association-adminAPI/src/main/java/com/feng/controller/FileController.java
+++ association-adminAPI/src/main/java/com/feng/controller/FileController.java
@@ -18,7 +18,7 @@ import javax.validation.Valid;
 @RestController
 @CrossOrigin
 @RequestMapping("/files")
-@Api(value = "社团管理系统后台文件管理文档接口",tags = "社团管理系统后台文件管理文档接口")
+@Api(value = "协会管理系统后台文件管理文档接口",tags = "协会管理系统后台文件管理文档接口")
 public class FileController {
     @Autowired
     private FileService fileService;
