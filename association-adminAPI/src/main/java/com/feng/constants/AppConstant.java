360167f5fb19563177e38fe67cf0433dadddb8c6
lijy78 <lijy78@asiainfo.com>
2026-01-08 11:31:28 +0800

    更改注释

--- association-adminAPI/src/main/java/com/feng/constants/AppConstant.java
+++ association-adminAPI/src/main/java/com/feng/constants/AppConstant.java
@@ -1,8 +1,5 @@
 package com.feng.constants;
 
-/**
- * Created by rf on 2019/5/4.
- */
 public class AppConstant {
     public static final String userSessionPrefix="userSession:";
     public static final String userTokenPrefix="userToken:";
